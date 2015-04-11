32f58def7c84d954fd7bfa6f02897efe61dcfd90
Joshua Moore <joshua.j.moore@gmail.com>
2015-04-11 12:07:36 -0700

    Shared Preference registration code. Not thoroughly tested yet.

--- app/src/main/java/com/uwt/strugglebus/geotracker/MainActivity.java
+++ app/src/main/java/com/uwt/strugglebus/geotracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.uwt.strugglebus.geotracker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,8 +15,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent login = new Intent(this, Login.class);
-        startActivity(login);
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.SHARED_PREFERENCES), Context.MODE_PRIVATE);
+        int uid = prefs.getInt("uid", -1);
+        if(uid != -1) { //user exists in shared prefs
+            Intent registration = new Intent(this, Registration.class);
+            //TODO: create and pass through a new User
+            startActivity(registration);
+        } else {
+            Intent login = new Intent(this, Login.class);
+            startActivity(login);
+        }
+
     }
 
 
@@ -22,7 +33,6 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        new Eula(this).show();
         return true;
     }
 
--- app/src/main/java/com/uwt/strugglebus/geotracker/Registration.java
+++ app/src/main/java/com/uwt/strugglebus/geotracker/Registration.java
@@ -1,11 +1,19 @@
 package com.uwt.strugglebus.geotracker;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class Registration extends ActionBarActivity {
@@ -20,6 +28,48 @@ public class Registration extends ActionBarActivity {
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        Button accept = (Button) findViewById(R.id.reg_accept);
+        Button cancel = (Button) findViewById(R.id.reg_cancel);
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText) findViewById(R.id.reg_email)).getText().toString();
+                String password = ((EditText) findViewById(R.id.reg_password)).getText().toString();
+                String confirm_password = ((EditText) findViewById(R.id.reg_confirm_password)).getText().toString();
+                String question = ((Spinner) findViewById(R.id.question_spinner)).getSelectedItem().toString();
+                String answer = ((EditText) findViewById(R.id.security_answer)).getText().toString();
+                //check to see if there is valid input TODO: test this if statement
+                if(!email.equals(null) && !password.equals(null) && !confirm_password.equals(null)
+                        && !answer.equals(null) && confirm_password.equals(password)) {
+                    //get unique id, and put into db
+                    //save user data in shared preferences
+                    SharedPreferences prefs = getSharedPreferences(getString(R.string.SHARED_PREFERENCES)
+                            , Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    //change to unique id once db webservice is up
+                    editor.putInt("uid",0);
+                    editor.putString(getString(R.string.email), email);
+                    editor.putString(getString(R.string.password), password);
+                    editor.putString(getString(R.string.security_q), question);
+                    editor.putString(getString(R.string.security_a), answer);
+                    editor.commit();
+                    //switch to eula
+                    Intent eula = new Intent(getApplicationContext(), Eula.class);
+                    startActivity(eula);
+                    finish();
+                }
+                Toast.makeText(getApplicationContext(),R.string.invalid , Toast.LENGTH_LONG).show();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(getApplicationContext(), Login.class);
+                startActivity(login);
+                finish();
+            }
+        });
     }
 
 
