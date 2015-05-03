81069e13b3000b7fae5fc06c3638449399fbf70f
Joshua Moore <joshua.j.moore@gmail.com>
2015-05-03 12:47:30 -0700

    changes to my account, still in progress

--- app/src/main/java/com/uwt/strugglebus/geotracker/View/MyAccount.java
+++ app/src/main/java/com/uwt/strugglebus/geotracker/View/MyAccount.java
@@ -1,16 +1,25 @@
 package com.uwt.strugglebus.geotracker.View;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Button;
 
 import com.uwt.strugglebus.geotracker.Controller.User;
 import com.uwt.strugglebus.geotracker.Model.LocationLog;
 import com.uwt.strugglebus.geotracker.R;
+
+import java.util.Calendar;
 
 /**
  * The MyAccount class holding all of the information for the user's data.
@@ -26,9 +35,9 @@ public class MyAccount extends ActionBarActivity {
         mLocationLog = new LocationLog();
         setContentView(R.layout.activity_my_account);
         TextView email = (TextView) findViewById(R.id.account_email);
-        TextView password = (TextView) findViewById(R.id.account_password);
-        TextView question = (TextView) findViewById(R.id.account_question);
-        TextView answer = (TextView) findViewById(R.id.account_answer);
+//        TextView password = (TextView) findViewById(R.id.account_password);
+//        TextView question = (TextView) findViewById(R.id.account_question);
+//        TextView answer = (TextView) findViewById(R.id.account_answer);
 
         SharedPreferences prefs = getSharedPreferences(getString(R.string.SHARED_PREFERENCES),
                 getApplicationContext().MODE_PRIVATE);
@@ -39,11 +48,49 @@ public class MyAccount extends ActionBarActivity {
         String mAnswer = prefs.getString(getString(R.string.security_a), "answer");
 
         email.setText(mEmail);
-        password.setText(mPass);
-        question.setText(mQuestion);
-        answer.setText(mAnswer);
+//        password.setText(mPass);
+//        question.setText(mQuestion);
+//        answer.setText(mAnswer);
 
         mUser = new User(uid, mEmail, mPass, mQuestion, mAnswer);
+
+        Button startDate = (Button) findViewById(R.id.start_date_button);
+        Button endDate = (Button) findViewById(R.id.end_date_button);
+        Button startTime = (Button) findViewById(R.id.start_time_button);
+        Button endTime = (Button) findViewById(R.id.end_time_button);
+        final Activity that = this;
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment frag = new DatePickerFragment();
+                Bundle b = new Bundle();
+                frag.setArguments(b);
+                frag.show(that.getFragmentManager(), "datePicker");
+            }
+        });
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment frag = new DatePickerFragment();
+                frag.show(that.getFragmentManager(), "datePicker");
+            }
+        });
+
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment frag = new TimePickerFragment();
+                frag.show(that.getFragmentManager(), "timePicker");
+            }
+        });
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment frag = new TimePickerFragment();
+                frag.show(that.getFragmentManager(), "timePicker");
+            }
+        });
+
     }
 
 
@@ -82,4 +129,6 @@ public class MyAccount extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
