65e4e4f84bc90a570d64dab97de3a84be6bfd08b
Josh M <joshua.j.moore@gmail.com>
2015-06-01 16:02:40 -0700

    where is my mind

--- app/src/main/java/com/uwt/strugglebus/geotracker/Model/LocationBroadcastReceiver.java
+++ app/src/main/java/com/uwt/strugglebus/geotracker/Model/LocationBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.uwt.strugglebus.geotracker.Model;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.uwt.strugglebus.geotracker.Model.Tracker;
@@ -16,11 +17,10 @@ public class LocationBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
             // Set the alarm here.
+            Log.i("fused", "Broadcast reciever");
             context.startService(new Intent(context, Tracker2.class));
-            //Tracker.setServiceAlarm(context, true);
         }
     }
 }
